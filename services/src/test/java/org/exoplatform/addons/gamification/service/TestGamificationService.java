0239faa69f34581c82d3de02f83cf00730fddfc1
AmineChalouati <machalouati@exoplatform.com>
2019-06-25 17:03:13 +0200

    US 5: *Reformat code *Optimize import

--- services/src/test/java/org/exoplatform/addons/gamification/service/TestGamificationService.java
+++ services/src/test/java/org/exoplatform/addons/gamification/service/TestGamificationService.java
@@ -1,203 +1,196 @@
 package org.exoplatform.addons.gamification.service;
+
+import java.util.Date;
+import java.util.List;
+
 import org.exoplatform.addons.gamification.entities.domain.effective.GamificationActionsHistory;
+import org.exoplatform.addons.gamification.service.dto.configuration.RuleDTO;
 import org.exoplatform.addons.gamification.service.effective.GamificationService;
 import org.exoplatform.addons.gamification.storage.dao.GamificationHistoryDAO;
 import org.exoplatform.addons.gamification.test.AbstractServiceTest;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
-import org.exoplatform.addons.gamification.service.dto.configuration.RuleDTO;
 import org.exoplatform.social.core.identity.model.Identity;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
 
 public class TestGamificationService extends AbstractServiceTest {
-    private static final String GAMIFICATION_DOMAIN  ="TeamWork";
-    private static final String RULE_NAME      ="Update a new Task";
-    /*Receiver */
-    private static final String TEST_USER_RECEIVER = "55";
-    /*Sender */
-    private static final String TEST_USER_SENDER = "1";
-    /*Link to the activity stream */
-    private static final String TEST_LINK_ACTIVITY = "/portal/intranet//activity?id=245590";
+  private static final String    GAMIFICATION_DOMAIN = "TeamWork";
 
-    private static final String TEST_GLOBAL_SCORE = "245590";
-    private GamificationHistoryDAO gamificationHistoryDAO;
+  private static final String    RULE_NAME           = "Update a new Task";
 
-//    private Identity actorIdentity = identityManager.getIdentity(TEST_USER_SENDER, false);
+  /* Receiver */
+  private static final String    TEST_USER_RECEIVER  = "55";
 
-    private static final Log LOG = ExoLogger.getLogger(GamificationService.class);
-    private RuleDTO ruleDTO= new RuleDTO();
+  /* Sender */
+  private static final String    TEST_USER_SENDER    = "1";
 
-    // Needed for exchange points between users
-    Identity testUserReceiver = new Identity(TEST_USER_RECEIVER);
-    Identity testUserSender = new Identity(TEST_USER_SENDER);
+  /* Link to the activity stream */
+  private static final String    TEST_LINK_ACTIVITY  = "/portal/intranet//activity?id=245590";
 
-    public GamificationActionsHistory build(RuleDTO ruleDto, String testUserSender, String testUserReceiver, String TEST_LINK_ACTIVITY) throws Exception{
+  private static final String    TEST_GLOBAL_SCORE   = "245590";
 
-        GamificationActionsHistory aHistory = null;
-try{
-        // Build only an entry when a rule enable and exist
-        if (ruleDto != null) {
-            aHistory = new GamificationActionsHistory();
-            aHistory.setActionScore(ruleDto.getScore());
-            try {
-                aHistory.setGlobalScore( TestcomputeTotalScore(testUserSender) + ruleDto.getScore());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            aHistory.setDate(new Date());
-            aHistory.setUserSocialId(testUserSender);
+  private static final Log       LOG                 = ExoLogger.getLogger(GamificationService.class);
 
-            aHistory.setActionTitle(RULE_NAME);
-            aHistory.setDomain(GAMIFICATION_DOMAIN);
-            aHistory.setReceiver(testUserReceiver);
-            aHistory.setObjectId(TEST_LINK_ACTIVITY);
-            // Set update metadata
-            aHistory.setLastModifiedDate(new Date());
-            aHistory.setLastModifiedBy("Gamification Inner Process");
-            // Set create metadata
-            aHistory.setCreatedBy("Gamification Inner Process");
-        }}
-catch (Exception e) {
-    LOG.error("Error to run GamificationActionsHistory build {}", aHistory, e);        }
-        return aHistory;
-    }
+  // private Identity actorIdentity =
+  // identityManager.getIdentity(TEST_USER_SENDER, false);
+  // Needed for exchange points between users
+  Identity                       testUserReceiver    = new Identity(TEST_USER_RECEIVER);
 
-    public void testSaveActionHistory() {
-        GamificationActionsHistory aHistory = null;
+  Identity                       testUserSender      = new Identity(TEST_USER_SENDER);
 
+  private GamificationHistoryDAO gamificationHistoryDAO;
+
+  private RuleDTO                ruleDTO             = new RuleDTO();
+
+  public GamificationActionsHistory build(RuleDTO ruleDto,
+                                          String testUserSender,
+                                          String testUserReceiver,
+                                          String TEST_LINK_ACTIVITY) throws Exception {
+
+    GamificationActionsHistory aHistory = null;
+    try {
+      // Build only an entry when a rule enable and exist
+      if (ruleDto != null) {
+        aHistory = new GamificationActionsHistory();
+        aHistory.setActionScore(ruleDto.getScore());
         try {
-            if (aHistory !=null){
-
-                assertNotNull(aHistory);
-                gamificationService.build(ruleDTO,TEST_USER_SENDER,TEST_USER_RECEIVER,TEST_LINK_ACTIVITY);
-                gamificationService.saveActionHistory(aHistory);
-
-            }
-
-
+          aHistory.setGlobalScore(TestcomputeTotalScore(testUserSender) + ruleDto.getScore());
         } catch (Exception e) {
-            LOG.error("Error to save the following GamificationActionsHistory entry {}", aHistory, e);        }
-
-    }
-
-
-    //Needed for the computed score
-
-    public long TestcomputeTotalScore(String testUserSender) throws Exception{
-        try {
-             gamificationHistoryDAO.computeTotalScore(testUserSender);
+          e.printStackTrace();
         }
-        catch (Exception e) {
-            LOG.error("Error in user total score {}",testUserSender , e);        }
-        return gamificationHistoryDAO.computeTotalScore(testUserSender);
+        aHistory.setDate(new Date());
+        aHistory.setUserSocialId(testUserSender);
 
+        aHistory.setActionTitle(RULE_NAME);
+        aHistory.setDomain(GAMIFICATION_DOMAIN);
+        aHistory.setReceiver(testUserReceiver);
+        aHistory.setObjectId(TEST_LINK_ACTIVITY);
+        // Set update metadata
+        aHistory.setLastModifiedDate(new Date());
+        aHistory.setLastModifiedBy("Gamification Inner Process");
+        // Set create metadata
+        aHistory.setCreatedBy("Gamification Inner Process");
+      }
+    } catch (Exception e) {
+      LOG.error("Error to run GamificationActionsHistory build {}", aHistory, e);
+    }
+    return aHistory;
+  }
+
+  public void testSaveActionHistory() {
+    GamificationActionsHistory aHistory = null;
+
+    try {
+      if (aHistory != null) {
+
+        assertNotNull(aHistory);
+        gamificationService.build(ruleDTO, TEST_USER_SENDER, TEST_USER_RECEIVER, TEST_LINK_ACTIVITY);
+        gamificationService.saveActionHistory(aHistory);
+
+      }
+
+    } catch (Exception e) {
+      LOG.error("Error to save the following GamificationActionsHistory entry {}", aHistory, e);
     }
 
+  }
 
+  // Needed for the computed score
 
-    public void testAddActivityOnUserStream() throws Exception {
+  public long TestcomputeTotalScore(String testUserSender) throws Exception {
+    try {
+      gamificationHistoryDAO.computeTotalScore(testUserSender);
+    } catch (Exception e) {
+      LOG.error("Error in user total score {}", testUserSender, e);
+    }
+    return gamificationHistoryDAO.computeTotalScore(testUserSender);
 
-        try{
-        Identity maryIdentity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "mary", false);}
-        catch (Exception e) {
-            LOG.error("Error in add activity {}", e);        }
+  }
 
+  public void testAddActivityOnUserStream() throws Exception {
+
+    try {
+      Identity maryIdentity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "mary", false);
+    } catch (Exception e) {
+      LOG.error("Error in add activity {}", e);
     }
 
+  }
 
+  public GamificationActionsHistory FindLatestActionHistoryBySocialId(String TEST_USER_SENDER) throws Exception {
+    List<GamificationActionsHistory> aHistory = null;
 
-    public GamificationActionsHistory FindLatestActionHistoryBySocialId(String TEST_USER_SENDER)throws Exception {
-        List<GamificationActionsHistory>  aHistory = null;
+    try {
 
+      aHistory = gamificationHistoryDAO.findActionsHistoryByUserId(TEST_USER_SENDER);
+      return (aHistory != null && !aHistory.isEmpty()) ? aHistory.get(0) : null;
+
+    } catch (Exception e) {
+      fail("Error to List an actionHistory associated to userId ", e);
+    }
+    return null;
+
+  }
+
+  public void testFindLatestActionHistoryBySocialId() {
+    List<GamificationActionsHistory> aHistory = null;
+
+    try {
+
+      if (aHistory != null) {
+        gamificationService.findLatestActionHistoryBySocialId(TEST_USER_RECEIVER);
+        gamificationService.findLatestActionHistoryBySocialId(TEST_USER_SENDER);
+
+        assertTrue((aHistory.size() == 1));
+        GamificationActionsHistory result = aHistory.iterator().next();
+        assertEquals(TEST_USER_SENDER, result.getUserSocialId());
+        assertEquals("updateTask", result.getActionTitle());
+        assertEquals("TeamWork", result.getDomain());
+        assertEquals("/portal/intranet/activity?id=245590", result.getObjectId());
+        assertEquals(TEST_GLOBAL_SCORE, result.getGlobalScore());
+        assertEquals(TEST_USER_RECEIVER, result.getReceiver());
+
+      }
+
+    } catch (Exception e) {
+      fail("Error to save an actionHistory entry ", e);
+    }
+
+  }
+
+  public void testbuild(RuleDTO ruleDto,
+                        String testUserSender,
+                        String testUserReceiver,
+                        String TEST_LINK_ACTIVITY) throws Exception {
+
+    GamificationActionsHistory aHistory = null;
+    try {
+      // Build only an entry when a rule enable and exist
+      if (ruleDto != null) {
+        aHistory = new GamificationActionsHistory();
+        aHistory.setActionScore(ruleDto.getScore());
         try {
-
-
-
-
-                aHistory=   gamificationHistoryDAO.findActionsHistoryByUserId(TEST_USER_SENDER);
-                return (aHistory != null && !aHistory.isEmpty()) ? aHistory.get(0) : null;
-
-
-
-        }
-        catch (Exception e) {
-            fail("Error to List an actionHistory associated to userId ",e);
-        }
-        return null;
-
-
-    }
-
-
-    public void testFindLatestActionHistoryBySocialId() {
-        List<GamificationActionsHistory> aHistory = null;
-
-        try {
-
-            if (aHistory != null) {
-                gamificationService.findLatestActionHistoryBySocialId(TEST_USER_RECEIVER);
-                gamificationService.findLatestActionHistoryBySocialId(TEST_USER_SENDER);
-
-
-                assertTrue((aHistory.size() == 1));
-                GamificationActionsHistory result = aHistory.iterator().next();
-                assertEquals(TEST_USER_SENDER,result.getUserSocialId());
-                assertEquals("updateTask",result.getActionTitle());
-                assertEquals("TeamWork",result.getDomain());
-                assertEquals("/portal/intranet/activity?id=245590",result.getObjectId());
-                assertEquals(TEST_GLOBAL_SCORE,result.getGlobalScore());
-                assertEquals(TEST_USER_RECEIVER,result.getReceiver());
-
-
-
-
-            }
-
-
+          aHistory.setGlobalScore(TestcomputeTotalScore(testUserSender) + ruleDto.getScore());
         } catch (Exception e) {
-            fail("Error to save an actionHistory entry ",e);
+          e.printStackTrace();
         }
+        aHistory.setDate(new Date());
+        aHistory.setUserSocialId(testUserSender);
 
+        aHistory.setActionTitle(RULE_NAME);
+        aHistory.setDomain(GAMIFICATION_DOMAIN);
+        aHistory.setReceiver(testUserReceiver);
+        aHistory.setObjectId(TEST_LINK_ACTIVITY);
+        // Set update metadata
+        aHistory.setLastModifiedDate(new Date());
+        aHistory.setLastModifiedBy("Gamification Inner Process");
+        // Set create metadata
+        aHistory.setCreatedBy("Gamification Inner Process");
+      }
+    } catch (Exception e) {
+      fail("Error to test an actionHistory entry ", e);
     }
 
-
-    public void testbuild(RuleDTO ruleDto, String testUserSender, String testUserReceiver, String TEST_LINK_ACTIVITY) throws Exception{
-
-        GamificationActionsHistory aHistory = null;
-try{
-        // Build only an entry when a rule enable and exist
-        if (ruleDto != null) {
-            aHistory = new GamificationActionsHistory();
-            aHistory.setActionScore(ruleDto.getScore());
-            try {
-                aHistory.setGlobalScore( TestcomputeTotalScore(testUserSender) + ruleDto.getScore());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            aHistory.setDate(new Date());
-            aHistory.setUserSocialId(testUserSender);
-
-            aHistory.setActionTitle(RULE_NAME);
-            aHistory.setDomain(GAMIFICATION_DOMAIN);
-            aHistory.setReceiver(testUserReceiver);
-            aHistory.setObjectId(TEST_LINK_ACTIVITY);
-            // Set update metadata
-            aHistory.setLastModifiedDate(new Date());
-            aHistory.setLastModifiedBy("Gamification Inner Process");
-            // Set create metadata
-            aHistory.setCreatedBy("Gamification Inner Process");
-        }} catch (Exception e) {
-    fail("Error to test an actionHistory entry ",e);
-}
-
-
-
-    }
-
-
-
+  }
 
 }
--- services/src/test/java/org/exoplatform/addons/gamification/test/AbstractServiceTest.java
+++ services/src/test/java/org/exoplatform/addons/gamification/test/AbstractServiceTest.java
@@ -1,4 +1,5 @@
 package org.exoplatform.addons.gamification.test;
+
 import org.exoplatform.addons.gamification.service.effective.GamificationService;
 import org.exoplatform.commons.persistence.impl.EntityManagerService;
 import org.exoplatform.commons.testing.BaseExoTestCase;
@@ -13,53 +14,53 @@ import org.exoplatform.social.core.manager.RelationshipManager;
 import org.exoplatform.social.core.space.spi.SpaceService;
 
 @ConfiguredBy({
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.gamification.component.core.test.configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.gamification.component.core.test.configuration.xml"),
 
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.portal-configuration.xml"),
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.identity-configuration.xml"),
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.jcr-configuration.xml"),
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.gamification.test.portal-configuration.xml"),
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.gamification.test.jcr-configuration.xml"),
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.XXXXXXgamification.component.core.test.application.registry.configuration.xml"),
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/component.search.configuration.xml"),
-        @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/gamification-test-configuration.xml")
-})
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.portal-configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.identity-configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.jcr-configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.gamification.test.portal-configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.gamification.test.jcr-configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.XXXXXXgamification.component.core.test.application.registry.configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/component.search.configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/gamification-test-configuration.xml") })
 public class AbstractServiceTest extends BaseExoTestCase {
 
-    protected SpaceService spaceService;
-    protected IdentityManager identityManager;
-    protected RelationshipManager relationshipManager;
-    protected ActivityManager activityManager;
-    protected EntityManagerService entityManagerService;
-    protected GamificationService gamificationService;
+  protected SpaceService         spaceService;
 
+  protected IdentityManager      identityManager;
 
-    @Override
-    protected void setUp() throws Exception {
-        begin();
+  protected RelationshipManager  relationshipManager;
 
-        identityManager = CommonsUtils.getService(IdentityManager.class);
-        activityManager =  CommonsUtils.getService(ActivityManager.class);
-        relationshipManager = CommonsUtils.getService(RelationshipManager.class);
-        spaceService = CommonsUtils.getService(SpaceService.class);
-        entityManagerService = CommonsUtils.getService(EntityManagerService.class);
+  protected ActivityManager      activityManager;
 
-        identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "root", false);
-        identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "john", true);
-        identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "mary", true);
-        identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "demo", true);
-        gamificationService = CommonsUtils.getService(GamificationService.class);
+  protected EntityManagerService entityManagerService;
 
-    }
+  protected GamificationService  gamificationService;
 
+  @Override
+  protected void setUp() throws Exception {
+    begin();
 
-    @Override
-    protected void tearDown() throws Exception {
+    identityManager = CommonsUtils.getService(IdentityManager.class);
+    activityManager = CommonsUtils.getService(ActivityManager.class);
+    relationshipManager = CommonsUtils.getService(RelationshipManager.class);
+    spaceService = CommonsUtils.getService(SpaceService.class);
+    entityManagerService = CommonsUtils.getService(EntityManagerService.class);
 
+    identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "root", false);
+    identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "john", true);
+    identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "mary", true);
+    identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "demo", true);
+    gamificationService = CommonsUtils.getService(GamificationService.class);
 
-        //
-        end();
-    }
+  }
 
+  @Override
+  protected void tearDown() throws Exception {
+
+    //
+    end();
+  }
 
 }
