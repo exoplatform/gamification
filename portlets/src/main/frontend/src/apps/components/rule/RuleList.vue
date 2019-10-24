e240242011be4e5d2e78b2328c399d42edc49908
mkrout <mkrout@exoplatform.com>
2019-10-24 17:07:49 +0200

    Manage rule: Fix alert dissmiss and style (#72)
    filter and search section fix
    improve rule table style

--- portlets/src/main/frontend/src/apps/components/rule/RuleList.vue
+++ portlets/src/main/frontend/src/apps/components/rule/RuleList.vue
@@ -36,7 +36,7 @@
 
                             <a class="uiIconClose pull-right" v-on:click.prevent="collapseButtonn(rule)"></a>
 
-                            <span class="PopupTitle popupTitle">{{ this.$t('exoplatform.gamification.gamificationinformation.domain.popupedit') }}</span>
+                            <span class="PopupTitle popupTitle">{{ this.$t('exoplatform.gamification.gamificationinformation.rule.popupedit') }}</span>
                         </div>
                         <div class="PopupContent popupContent">
                             <form id="titleInputGroup">
@@ -320,6 +320,9 @@ export default {
         onSave(rule) {
             this.$emit('save', rule);
             this.editedrule = {};
+            if (this.isShown) {
+                this.closeAlert(".alert")
+            }
         },
         onCancel(rule) {
             this.editedrule = {};
@@ -354,14 +357,7 @@ export default {
     display: flex;
     flex-direction: row-reverse;
     float: right;
-    margin-top: 18px;
-}
-
-.filterWithIcon {
-    display: flex;
-    flex-direction: row-reverse;
-    float: right;
-    margin-top: 18px;
+    margin-top: 17px;
 }
 
 i.uiIconSearch.uiIconLightGray {
@@ -422,7 +418,7 @@ i.uiIconSearch.uiIconLightGray {
 }
 
 input[type="text"] {
-    height: 35px;
+    height: 37px;
     margin: auto;
 }
 
@@ -622,7 +618,6 @@ div#collapseTwo {
 
 .card-body label {
     display: block;
-    margin-right: 10px;
 }
 
 form {
@@ -690,7 +685,6 @@ button, [type="button"], [type="reset"], [type="submit"] {
     -webkit-appearance: button;
     align-content: stretch;
     padding: 8px 25px;
-   /* margin-left: 500px; */
 }
 
 input[type="checkbox"] {
@@ -889,9 +883,6 @@ i.uiIconClose.uiIconBlue {
     border-radius: 5px;
     box-shadow: none;
     margin-left: 20px;
-    height:40px;
-    width:352px;
-    margin-top: -7px;
 }
 
 select:focus {
@@ -906,22 +897,8 @@ select:focus {
 select.mb-4.select-event {
     margin: 0 !important;
     width: 100%;
-    height: 40px;
 }
-.filterWithIcon{
-    display: flex;
-    flex-direction: row-reverse;
-    float: right;
-    margin: 10px;
-    font-size: 15px;
-    height: 35px;
-    border: 2px Solid #e1e8ee;
-    border-radius: 5px;
-    box-shadow: none;
-    width: 90px;
-    text-overflow: ellipsis;
-    margin-top: 18px;
-}
+
 .action-bar.dropdown.filterWithIcon> a.actionIcon.dropdown-toggle {
     box-shadow: none;
     border: none;
@@ -933,9 +910,8 @@ select.mb-4.select-event {
 }
 form.switchEnabled, form#domainSelectboxGroup {
     display: inline-flex;
-    min-width: 38%;
-    margin-top: 30px;
-
+    min-width: 40%;
+    margin: 20px 5px;
 }
 button.btn-primary.pull-right {
     border-radius: 0.25rem;
@@ -946,17 +922,28 @@ button.btn-primary.pull-right {
     display: flex;
     flex-direction: row-reverse;
     float: right;
-    margin-top: 21px;
+    margin-top: 17px;
     margin-right: 12px;
     }
+.filter-bar select.mb-4 {
+    word-wrap: normal;
+    border: 2px Solid #e1e8ee;
+    border-radius: 5px;
+    margin: auto;
+    outline: none;
+    height: 36px;
+    width: auto;
+    padding: 0 10px;
+}
     .btn-primary.disabled, .btn-primary:disabled {
      background-color: #afc9e5; 
      background-image: none;
 }
-
-.popupContent label.switch {
-    margin-left: 10px;
-    margin-top: -8px;
+.PopupContent.popupContent select {
+    height: 40px;
 }
-
+form.switchEnabled label.switch {
+    margin-left: 21px;
+    margin-top: -7px;
+}
 </style>
