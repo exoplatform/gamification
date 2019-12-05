e686867f9778f41b03cbe0fab55787eb3b8e6ca7
wafakd <wkdous@exoplatform.com>
2019-12-05 14:16:11 +0100

    PLF-8495: fix title font size

--- portlets/src/main/frontend/src/apps/components/BadgeApp.vue
+++ portlets/src/main/frontend/src/apps/components/BadgeApp.vue
@@ -56,4 +56,36 @@ develop
     .dropdown-menu {
         font-size: inherit;
     }
+
+
+    .uiBreadcumbsNavigationPortlet .uiBreadcumbsNavigations .navItemSelected {
+        font-size: 14px;
+    }
+    table.uiGrid.table.table-hover.uiGrid.table tr th {
+        font-size: 16px;
+    }
+    .uiGrid.table td {
+        font-size: 15px;
+    }
+    .uiSpaceNavigationPortlet .searchInput:before {
+        top: 27px;
+    }
+    /* Fix/ PLF-8495 */
+    .tooltip.fade.bottom.in {
+        opacity: 1 !important;
+    }
+    .uiCompanyNavigationPortlet .uiCompanyNavigations > li > a, h5 {
+        box-sizing: content-box;
+        font-family: Helvetica, arial, sans-serif;
+    }
+    a:hover{
+        text-decoration: none;
+        background-color: transparent;
+        -webkit-text-decoration-skip: objects;
+    }
+    .uiCompanyNavigationPortlet .title, .uiSpaceNavigationPortlet .title {
+        font-size: 14px;
+    }
+    
+    
 </style>
